8c991d5d0b98a1e12db4c3390ff159e70c58ff8f
Jean-Claude Jung <jjung@adobe.com>
2019-04-30 11:10:13 +0200

    minor improvements

--- src/get_md_link.py
+++ src/get_md_link.py
@@ -5,9 +5,6 @@ from urllib import pathname2url
 import os
 
 query = sys.argv[1]
-
 file_name = os.path.basename(query)
-
 output = '['+file_name+'](' + pathname2url(file_name) + ')'
-
 sys.stdout.write(output)
