06e860434e608e7f7f7f9713c416f4059232e833
Acidham <acidham@gmail.com>
2019-08-19 20:51:30 +0200

    Make Evernote Optional

--- src/search_actions.py
+++ src/search_actions.py
@@ -19,48 +19,56 @@ ACTIONS = [
         "title": "Back",
         "subtitle": "Back to Search with query: {0}".format(back_query),
         "arg": "{0}|{1}".format("back", query),
-        "icon": "icons/back.png"
+        "icon": "icons/back.png",
+        "visible": True
     },
     {
         "title": "Markdown Link",
         "subtitle": "Copy MD Link for \"{0}\" to the Clipboard".format(note_title),
         "arg": "{0}|[{1}]({2})".format("link", note_title, note_path),
-        "icon": "icons/link.png"
+        "icon": "icons/link.png",
+        "visible": True
     },
     {
         "title": "Evernote",
         "subtitle": "Export \"{0}\" to Evernote".format(note_title),
         "arg": "{0}|{1}".format("evernote", note_path),
-        "icon": "icons/evernote.png"
+        "icon": "icons/evernote.png",
+        "visible": Tools.getEnv("evernote_auth_token")
     },
     {
         "title": "Marked 2",
         "subtitle": "Open Preview in Marked 2",
         "arg": "{0}|{1}".format("marked", note_path),
-        "icon": "icons/marked.png"
+        "icon": "icons/marked.png",
+        "visible": True
     },
     {
         "title": "Url Scheme",
         "subtitle": "Copy Url Scheme as Markdown Link to Clipboard",
         "arg": "{0}|{1}".format("urlscheme", note_path),
-        "icon": "icons/scheme.png"
+        "icon": "icons/scheme.png",
+        "visible": Tools.getEnv("url_scheme")
     },
     {
         "title": "Delete Note",
         "subtitle": "Delete \"{0}\". This action cannot be undone!".format(note_title),
         "arg": "{0}|{1}>{2}".format("delete", note_path, "test"),
-        "icon": "icons/delete.png"
+        "icon": "icons/delete.png",
+        "visible": True
     },
 ]
 
 # Generate ScriptFilter Output
 wf = Items()
 for a in ACTIONS:
-    wf.setItem(
-        title=a.get("title"),
-        subtitle=a.get("subtitle"),
-        arg=a.get("arg")
-    )
-    wf.setIcon(m_path=a.get("icon"), m_type="image")
-    wf.addItem()
+    val = a.get('visible')
+    if val:
+        wf.setItem(
+            title=a.get("title"),
+            subtitle=a.get("subtitle"),
+            arg=a.get("arg")
+        )
+        wf.setIcon(m_path=a.get("icon"), m_type="image")
+        wf.addItem()
 wf.write()
